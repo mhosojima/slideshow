a4ded5cdd417f7ea9258c7ebce8145be205d0d95
masahiko.hosojima <zero_001@qc5.so-net.ne.jp>
2019-02-13 15:10:17 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 細島誠彦 on 2019/02/13.
+//  Copyright © 2019 TransamManegemetnSystem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
